3457d8ead7071e41eb676e2f29e6c9d3c0dd34c4
David Kubec <davesade.42@gmail.com>
2018-04-10 11:16:48 +0200

    Changed Ubuntu to Amazon Linux.

--- main.tf
+++ main.tf
@@ -2,11 +2,20 @@
 ### We use the data value of the ami id found in inputs.tf, and the subnet_id defined in inputs.tf
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+
+  ami           = "${data.aws_ami.amazonlinux.id}"
   instance_type = "t2.micro"
   subnet_id     = "${var.SUBNET}"
+  key_name      = "mykey"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = ["${var.SECURITYGROUP}"]
 
   tags {
     Name = "HelloWorld"
   }
+
+  lifecycle {
+    create_before_destroy = "true"
+  }
+
 }
